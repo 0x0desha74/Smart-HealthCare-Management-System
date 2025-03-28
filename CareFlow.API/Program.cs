c4897d0bc8fd5d74c0a19ba4e1a3ea31a01acbd2
0x0desha74 <mudesha74@gmail.com>
2025-03-28 07:43:43 +0200

    Configure DbContext - Migration - Apply Migration

--- CareFlow.API/Program.cs
+++ CareFlow.API/Program.cs
@@ -1,9 +1,13 @@
 
+using CareFlow.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
 namespace CareFlow.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -14,8 +18,31 @@ namespace CareFlow.API
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string not found");
+                options.UseSqlServer(connectionString);
+            });
+
+
             var app = builder.Build();
 
+            var scop = app.Services.CreateScope();
+            var services = scop.ServiceProvider;
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+            try
+            {
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                await context.Database.MigrateAsync();
+            }
+            catch (Exception ex)
+            {
+
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(ex, "An error occurred during migration");
+
+            }
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
