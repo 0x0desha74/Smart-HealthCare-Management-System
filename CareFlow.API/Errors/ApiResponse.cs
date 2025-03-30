d92b44156ff1ba3c8ceb0ffb89acaa34bd7b5c42
0x0desha74 <mudesha74@gmail.com>
2025-03-30 18:05:36 +0200

    Create ApiResponse to handle Error Responses

---
+++ CareFlow.API/Errors/ApiResponse.cs
@@ -0,0 +1,28 @@
+﻿
+namespace CareFlow.API.Errors
+{
+    public class ApiResponse
+    {
+
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+
+        public ApiResponse(int statusCode, string? message)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        private string? GetDefaultMessageForStatusCode(int StatusCode)
+        {
+            return StatusCode switch
+            {
+                400 => "A bad request, you have made.",
+                404 => "Resource was not found.",
+                401 => "Authorized!, you are not.",
+                500 => "Errors are the path to the dark side. Errors lead to anger, anger leads to hate, hate leads to career change.",
+                _ => null
+            };
+        }
+    }
+}
--- CareFlow.Data/Interfaces/IUnitOfWork.cs
+++ CareFlow.Data/Interfaces/IUnitOfWork.cs
@@ -11,5 +11,6 @@ namespace CareFlow.Core.Interfaces
    public interface IUnitOfWork : IDisposable
     {
         IGenericRepository<T> Repository<T>() where T:BaseEntity;
+        Task<int> Complete();
     }
 }
