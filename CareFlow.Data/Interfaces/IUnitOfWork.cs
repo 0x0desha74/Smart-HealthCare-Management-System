f01f203b6d9bbd74f1fae8fabb0741f1bd253bc6
0x0desha74 <mudesha74@gmail.com>
2025-03-30 07:13:54 +0200

    Implement UnitOfWork

---
+++ CareFlow.Data/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using CareFlow.Data.Entities;
+using CareFlow.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareFlow.Core.Interfaces
+{
+   public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<T> Repository<T>() where T:BaseEntity;
+    }
+}
---
+++ CareFlow.Repository/Repositories/UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using CareFlow.Core.Interfaces;
+using CareFlow.Data.Entities;
+using CareFlow.Data.Interfaces;
+using CareFlow.Repository.Data;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareFlow.Repository.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private Hashtable _repositories;
+        public UnitOfWork(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IGenericRepository<T> Repository<T>() where T : BaseEntity
+        {
+            if (_repositories is null)
+                _repositories = new Hashtable();
+            var type = typeof(T).Name;
+            if(!_repositories.ContainsKey(type))
+            {
+                var repository = new GenericRepository<T>(_dbContext);
+                _repositories.Add(type, repository);
+
+            }
+
+            return _repositories[type] as GenericRepository<T>;
+
+        }
+        public async Task<int> Complete()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+
+        
+    }
+}
