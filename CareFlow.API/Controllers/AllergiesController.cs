da82d175a655e76589d7e5a2932cca0ef643afed
0x0desha74 <mudesha74@gmail.com>
2025-04-06 22:53:39 +0200

    GetAllergiesForPatient Endpoint

--- CareFlow.API/Controllers/AllergiesController.cs
+++ CareFlow.API/Controllers/AllergiesController.cs
@@ -1,5 +1,6 @@
 ﻿using CareFlow.API.Errors;
 using CareFlow.Core.DTOs.Requests;
+using CareFlow.Core.DTOs.Response;
 using CareFlow.Core.Interfaces.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -17,12 +18,28 @@ namespace CareFlow.API.Controllers
             _allergyService = allergyService;
         }
 
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<AllergyToReturnDto>>> GetAllergiesForPatient(Guid patientId)
+        {
+            var allergies = await _allergyService.GetAllergiesForPatient(patientId);
+            if (allergies is null) return NotFound(new ApiResponse(404, "No allergies assigned for this patient"));
+            return Ok(allergies);
+        }
+
+
+
+
+
+
+
         [HttpPost]
         public async Task<ActionResult<AllergyDto>> Create(Guid patientId,AllergyDto model)
         {
             var allergy = await _allergyService.AddAllergyToPatient(patientId, model);
             return Ok(allergy);
         }
+
 
 
 
