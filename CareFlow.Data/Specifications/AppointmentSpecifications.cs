9f7bf744a64de1cebf98b2c42b641036f93969d9
0x0desha74 <mudesha74@gmail.com>
2025-05-05 20:24:35 +0300

    Searching for appointment using startData, endDate , status

--- CareFlow.Data/Specifications/AppointmentSpecifications.cs
+++ CareFlow.Data/Specifications/AppointmentSpecifications.cs
@@ -6,27 +6,26 @@ namespace CareFlow.Core.Specifications
     public class AppointmentSpecifications : BaseSpecification<Appointment>
     {
 
-        public AppointmentSpecifications(AppointmentFilterDto specParams,string userId)
+        public AppointmentSpecifications(AppointmentFilterDto specParams, string userId)
             : base(a =>
+
             (a.Doctor.AppUserId == userId || a.Patient.AppUserId == userId) &&
 
-            (
-            
-            string.IsNullOrEmpty(specParams.Search) ||
-            (a.Notes != null && a.Notes.ToLower().Contains(specParams.Search) ) ||
-            (a.Reason != null && a.Reason.ToLower().Contains(specParams.Search))
-            
-            ) &&
+            (string.IsNullOrEmpty(specParams.Search) ||
+            (a.Notes != null && a.Notes.ToLower().Contains(specParams.Search)) ||
+            (a.Reason != null && a.Reason.ToLower().Contains(specParams.Search))) &&
+
             //(( a.Doctor.FirstName + " " + a.Doctor.LastName).ToLower().Contains(specParams.Doctor) && a.Doctor.AppUserId == userId) ||
             //(( a.Patient.FirstName + " " + a.Patient.LastName).ToLower().Contains(specParams.Patient) && a.Patient.AppUserId == userId) ||
-            (
-            
-            ( 
-            string.IsNullOrEmpty(specParams.Clinic) ||( a.Clinic != null && a.Clinic.Name.ToLower().Contains(specParams.Clinic))) 
-            )
-                
+
+            ((string.IsNullOrEmpty(specParams.Clinic) || (a.Clinic != null && a.Clinic.Name.ToLower().Contains(specParams.Clinic)))) &&
+
+            (specParams.Status == null || a.Status == specParams.Status) &&
+            (!specParams.StartDate.HasValue || a.AppointmentDate >= specParams.StartDate) &&
+            (!specParams.EndDate.HasValue || a.AppointmentDate <= specParams.EndDate)
+
                  )
-         
+
         {
             AddIncludes(q => q.Include(a => a.Patient).ThenInclude(p => p.PhoneNumbers));
             AddIncludes(q => q.Include(a => a.Patient).ThenInclude(p => p.Allergies));
